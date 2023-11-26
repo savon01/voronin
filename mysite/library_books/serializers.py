1b697af4476c7284caa49d8496e27aa3ba994008
Victor Savonchik <savonvictor@gmail.com>
2023-11-26 17:36:50 +0300

    Получение списка книг

--- mysite/library_books/serializers.py
+++ mysite/library_books/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.response import Response
+
 from .models import Book
 
 
@@ -8,5 +10,21 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
 
+    def list(self, request):
+        # Получение списка книг
+        queryset = self.get_queryset()  # Получение QuerySet всех книг
+
+        # Применение фильтров, если они переданы в запросе
+        title = request.query_params.get('title')
+        author = request.query_params.get('author')
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+
+
+        # Сериализация и возврат ответа
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
